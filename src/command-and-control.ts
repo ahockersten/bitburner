00a7cdf263f446533752ae478d7e2724b5068471
Anders Höckersten <anders@hockersten.se>
2024-08-31 12:36:39 +0200

    WIP on new command and control scripts

--- src/command-and-control.ts
+++ src/command-and-control.ts
@@ -1,9 +1,31 @@
-// så idén är att bygga ett skript som håller koll på alla servrar
-// vi har. Vi kan då räkna ut vad som händer vid varje attack, dispatcha,
-// beräkna om, och sen attackera när det är dags. Vi kan också hålla
-// koll på flera servrar
 import { NS } from "@ns";
+import { findHackTargets } from "util/find-hack-targets";
+import { scanServers, ServerData } from "./util/scan-servers";
+import { maxPortsRequired } from "./util/max-ports-required";
 
 /** @param {NS} ns */
 export async function main(ns: NS): Promise<void> {
+  const minMoney = (ns.args[0] as number | undefined ?? 0) * 1_000_000;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const myHackingLevel = ns.getHackingLevel();
+    const portsRequired = maxPortsRequired(ns);
+    const purchasedServers = ns.getPurchasedServers();
+    const allServers = scanServers(ns);
+    const hackedServers = new Map<string, ServerData>();
+    for (const [server, data] of allServers.entries()) {
+      if (data.hackable &&
+        data.hackingLevelRequired < myHackingLevel &&
+        data.portsRequired < portsRequired) {
+        hackedServers.set(server, data);
+      }
+    }
+    const servers = findHackTargets(ns);
+    for (const [server, data] of servers.reverse()) {
+
+    }
+
+    await ns.sleep(1000);
+  }
+
 }
